914183c895cb4ed537198089b1db410bbc820e13
Junior Alves <jrwanderley17@gmail.com>
2022-09-27 20:14:14 -0300

    test: ensure CreateTicketController return statusCode 400 if not institution is provided

---
+++ src/presentation/controllers/ticket/create-ticket.spec.ts
@@ -0,0 +1,22 @@
+import { CreateTicketController } from './create-ticket'
+
+const makeSut = (): CreateTicketController => {
+  const sut = new CreateTicketController()
+  return sut
+}
+describe('Create Ticket Controller', () => {
+  it('should return 400 if no institution is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        title: 'any_title',
+        description: 'any_description',
+        price: 55.0,
+        category: 'any_category',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
