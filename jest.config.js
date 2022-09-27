f448a2ae702dba7c5f847e7eb7b94d0617be3f19
Junior Alves <jrwanderley17@gmail.com>
2022-09-27 19:19:30 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['<rootDir>/src/**/__tests__/*.ts'],
+}
