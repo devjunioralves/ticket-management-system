3bca9d650e75603abb61ef603d238ba09b8e4b66
Junior Alves <jrwanderley17@gmail.com>
2022-09-27 20:31:47 -0300

    feat: ensure CreateTicketController return MissingParamError with param required

--- src/presentation/controllers/ticket/create-ticket.ts
+++ src/presentation/controllers/ticket/create-ticket.ts
@@ -1,7 +1,16 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class CreateTicketController {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise((resolve) => resolve({ statusCode: 400, body: 'any_body' }))
+    const requiredFields = ['title', 'description', 'price', 'category', 'institution']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return new Promise((resolve) => resolve({ statusCode: 200, body: {} }))
   }
 }
