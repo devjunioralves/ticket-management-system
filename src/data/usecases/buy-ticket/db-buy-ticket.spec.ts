42850b772a06e5c55aec905987744365523c91a4
Junior Alves <jrwanderley17@gmail.com>
2022-09-29 19:22:45 -0300

    test: ensure DbBuyTicket return purchase details when success

--- src/data/usecases/buy-ticket/db-buy-ticket.spec.ts
+++ src/data/usecases/buy-ticket/db-buy-ticket.spec.ts
@@ -56,4 +56,29 @@ describe('DbBuyTicket useCase', () => {
       total: 55.0,
     })
   })
+  it('should return the purchase details when success', async () => {
+    const { sut } = makeSut()
+    const buyTicketRequest = {
+      ticketId: 'valid_ticket_id',
+      customerName: 'valid_customer_name',
+      customerId: 'valid_customer_id',
+      customerEmail: 'valid_customer_email',
+      customerMobile: 'valid_customer_mobile',
+      customerDocument: 'valid_customer_document',
+      paymentType: 'valid_payment_type',
+      total: 55.0,
+    }
+    const boughtTicket = await sut.buy(buyTicketRequest)
+    expect(boughtTicket).toEqual({
+      id: 'valid_id',
+      ticketId: 'valid_ticket_id',
+      customerName: 'valid_customer_name',
+      customerId: 'valid_customer_id',
+      customerEmail: 'valid_customer_email',
+      customerMobile: 'valid_customer_mobile',
+      customerDocument: 'valid_customer_document',
+      paymentType: 'valid_payment_type',
+      total: 55.0,
+    })
+  })
 })
