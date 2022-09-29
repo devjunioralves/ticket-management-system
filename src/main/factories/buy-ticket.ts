afaf3cf5b44a8a517c55ca5555297f32bd5f3932
Junior Alves <jrwanderley17@gmail.com>
2022-09-29 19:37:32 -0300

    feat: add factory to mount buy ticket object

---
+++ src/main/factories/buy-ticket.ts
@@ -0,0 +1,10 @@
+import { Controller } from '../../presentation/protocols/controller'
+import { BuyTicketMongoRepository } from '../../infra/db/mongodb/buy-ticket-repository/buy-ticket'
+import { DbBuyTicket } from '../../data/usecases/buy-ticket/db-buy-ticket'
+import { BuyTicketController } from '../../presentation/controllers/buy-ticket/buy-ticket'
+
+export const makeBuyTicketController = (): Controller => {
+  const buyTicketMongoRepository = new BuyTicketMongoRepository()
+  const dbBuyTicket = new DbBuyTicket(buyTicketMongoRepository)
+  return new BuyTicketController(dbBuyTicket)
+}
