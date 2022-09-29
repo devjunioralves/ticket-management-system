226372e09ea06833a5e010a5cad0c3d9df652ec8
Junior Alves <jrwanderley17@gmail.com>
2022-09-29 16:23:36 -0300

    feat: ensure ListTicket mount dependency tree

---
+++ src/main/factories/list-ticket.ts
@@ -0,0 +1,10 @@
+import { Controller } from '../../presentation/protocols/controller'
+import { TicketMongoRepository } from '../../infra/db/mongodb/ticket-repository/ticket'
+import { DbListTicket } from '../../data/usecases/list-ticket/db-list-ticket'
+import { ListTicketController } from '../../presentation/controllers/list-ticket/list-ticket'
+
+export const makeListTicketController = (): Controller => {
+  const ticketMongoRepository = new TicketMongoRepository()
+  const dbAddTicket = new DbListTicket(ticketMongoRepository)
+  return new ListTicketController(dbAddTicket)
+}
