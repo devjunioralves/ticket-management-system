fbb8babb8dacffce228f5a8f7ccb40a1a28d31f6
Junior Alves <jrwanderley17@gmail.com>
2022-09-30 15:10:49 -0300

    feat: ensure insert bought ticket correctly

--- src/infra/db/mongodb/buy-ticket-repository/buy-ticket.ts
+++ src/infra/db/mongodb/buy-ticket-repository/buy-ticket.ts
@@ -2,11 +2,17 @@ import { BuyTicketRepository } from '../../../../data/protocols/buy-ticket-repos
 import { BoughtTicketModel } from '../../../../domain/models/bought-ticket'
 import { BuyTicketModel } from '../../../../domain/usecases/buy-ticket'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class BuyTicketMongoRepository implements BuyTicketRepository {
   async buy(buyTicketModel: BuyTicketModel): Promise<BoughtTicketModel> {
     const buyTicketCollection = MongoHelper.getCollection('bought-tickets')
-    const transaction = await buyTicketCollection.insertOne(buyTicketModel)
+    const { insertedId } = await buyTicketCollection.insertOne({
+      ...buyTicketModel,
+      ticketId: new ObjectId(buyTicketModel.ticketId),
+      customerId: new ObjectId(buyTicketModel.customerId),
+    })
+    const transaction = await buyTicketCollection.findOne<Promise<BoughtTicketModel | any>>({ _id: insertedId })
     return MongoHelper.map(transaction)
   }
 }
