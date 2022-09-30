8dd3254aa7bc6e64f56313ea97c36caec5339fed
Junior Alves <jrwanderley17@gmail.com>
2022-09-30 15:11:13 -0300

    feat: add new route to buy ticket

--- src/main/routes/ticket-routes.ts
+++ src/main/routes/ticket-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { makeTicketController } from '../factories/add-ticket'
 import { makeListTicketController } from '../factories/list-ticket'
 import { adaptRoute } from '../adapters/express-routes-adapter'
+import { makeBuyTicketController } from '../factories/buy-ticket'
 
 export default (router: Router): void => {
   router.post('/ticket', adaptRoute(makeTicketController()))
   router.get('/ticket', adaptRoute(makeListTicketController()))
+  router.post('/ticket/buy', adaptRoute(makeBuyTicketController()))
 }
