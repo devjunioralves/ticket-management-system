197233f22f582521d6df23db567817839570d8b4
Junior Alves <jrwanderley17@gmail.com>
2022-10-03 19:26:04 -0300

    test: ensure add ticket and return tickets on success

---
+++ src/infra/db/mongodb/ticket-repository/ticket.spec.ts
@@ -0,0 +1,40 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { TicketMongoRepository } from './ticket'
+import { ObjectId } from 'mongodb'
+
+describe('Ticket mongo repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const boughtTicketCollection = MongoHelper.getCollection('tickets')
+    await boughtTicketCollection.deleteMany({})
+  })
+
+  const makeSut = (): TicketMongoRepository => {
+    return new TicketMongoRepository()
+  }
+
+  it('should add a ticket and return on success', async () => {
+    const sut = makeSut()
+    const ticket = await sut.add({
+      title: 'any_title',
+      description: 'any_description',
+      price: 55.0,
+      category: 'any_category',
+      institution: 'any_institution',
+    })
+    expect(ticket).toBeTruthy()
+    expect(ticket.id).toBeTruthy()
+    expect(ticket.title).toBe('any_title')
+    expect(ticket.description).toBe('any_description')
+    expect(ticket.price).toBe(55)
+    expect(ticket.category).toBe('any_category')
+    expect(ticket.institution).toBe('any_institution')
+  })
+})
